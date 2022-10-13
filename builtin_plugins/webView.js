59a049705b4c73d7d82b6720979c729e30ba283b
678435021 <amongusthem69@gmail.com>
2022-10-14 00:10:33 +0100

    Add settings for webview plugin

--- builtin_plugins/webView.js
+++ builtin_plugins/webView.js
@@ -6,17 +6,25 @@ const webServerPort = 3000;
 
 const load = (A) => {
 	A.bot.once('spawn', () => {
-		mineflayerViewer(A.bot, { port: webServerPort, version: A.bot.version }); // Start the viewing server on port 3000
+		mineflayerViewer(
+			A.bot, {
+				port: webServerPort,
+				version: A.bot.version,
+				firstPerson: A.settings.settings.config.config.config.mineflayer.webView.firstPerson
+			}
+		); // Start the viewing server on port 3000
 		logger.info(`Web viewer running on port ${logger.warn.color + ansi.color.italic}https://localhost:${webServerPort}%COLOR%`);
 
 		// Draw the path followed by the bot
-		const path = [A.bot.entity.position.clone()];
-		A.bot.on('move', () => {
-			if (path[path.length - 1].distanceTo(A.bot.entity.position) > 1) {
-				path.push(A.bot.entity.position.clone());
-				A.bot.viewer.drawLine('path', path);
-			}
-		});
+		if (A.settings.settings.config.config.config.mineflayer.webView.showTrail === true) {
+			const path = [A.bot.entity.position.clone()];
+			A.bot.on('move', () => {
+				if (path[path.length - 1].distanceTo(A.bot.entity.position) > 1) {
+					path.push(A.bot.entity.position.clone());
+					A.bot.viewer.drawLine('path', path);
+				}
+			});
+		}
 	});
 };
 
